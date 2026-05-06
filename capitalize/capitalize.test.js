c5d092775e89b1ef3d45193db50895f8bbf8d472
Ethan <ethancredo.ascc@outlook.com>
2026-05-06 16:47:49 +0800

    rename folders

--- capitalize/capitalize.test.js
+++
@@ -1,6 +0,0 @@
-const capitalize = require('./capitalize');
-
-test('takes a string and returns it with the first character capitalized', () => {
-    expect(capitalize('sagi')).toBe('Sagi');
-    expect(capitalize('sAGI')).toBe('SAGI');
-});
